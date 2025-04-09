77202b2402022d7318466be1a1310f05661bbeb7
Mvikram-34 <vikraamaditya2003@gmail.com>
2025-04-09 09:11:49 +0530

    Update Determine_Fame.py

--- app/Determine_Fame.py
+++ app/Determine_Fame.py
@@ -15,7 +15,15 @@ import unicodedata
 artists_data = pd.DataFrame()
 artists, artists_fame, unavailable_artists = [], [], []
 unique_artists = {}
-driver = webdriver.Chrome()
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
+
 
 
 def clean_string(text):
